2f2e478c1737270fed7156c6624a0108a2588512
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2021-12-07 10:50:10 +0000

    Updated trigger area

--- ChristmasTreeGame/Assets/Dan's Stuff/Dan's Scripts/GameEventScripts/TriggerArea.cs
+++ ChristmasTreeGame/Assets/Dan's Stuff/Dan's Scripts/GameEventScripts/TriggerArea.cs
@@ -5,15 +5,33 @@ using UnityEngine;
 public class TriggerArea : MonoBehaviour
 {
     public int id;
+    [SerializeField] private bool canSpawn = true;
+    private Collider tempCollider;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             Debug.Log("Player in range");
-            GameEvents.current.ItemHolderTrigerEnter(id);
+            if (canSpawn)
+            {
+                GameEvents.current.ItemHolderTrigerEnter(id);
+            }
+            
         }
         
 
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Decoration")
+        {
+            canSpawn = false;
+        }
+        else
+        {
+            canSpawn = true;
+        }
+        
     }
 
     private void OnTriggerExit(Collider other)
