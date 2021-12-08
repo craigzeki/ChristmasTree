39d8f056e752987191f76286a822d49f440c2e00
JonFieldhouse93 <75310581+JonFieldhouse93@users.noreply.github.com>
2021-12-08 12:22:22 +0000

    Imported prefabs into scene and started working on progress bar

---
+++ ChristmasTreeGame/Assets/Jon's Stuff/Scripts/ProgressBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient grad;
+    public Image fill;
+
+    public void SetComplete(int progress)
+    {
+        slider.maxValue = progress;
+        slider.value = progress;
+        fill.color = grad.Evaluate(1f);
+    }
+
+    public void CurrentStatus(int progress)
+    {
+        slider.value = progress;
+        fill.color = grad.Evaluate(slider.normalizedValue);
+    }
+}
