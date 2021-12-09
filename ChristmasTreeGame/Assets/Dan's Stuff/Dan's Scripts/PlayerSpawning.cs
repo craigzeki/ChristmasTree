fdf05a878519211ba7774e30cc1b956fd5d1ac38
craigzeki <craig.zeki@gmail.com>
2021-12-09 13:54:01 +0000

    Fixed spawning

--- ChristmasTreeGame/Assets/Dan's Stuff/Dan's Scripts/PlayerSpawning.cs
+++ ChristmasTreeGame/Assets/Dan's Stuff/Dan's Scripts/PlayerSpawning.cs
@@ -7,11 +7,22 @@ public class PlayerSpawning : MonoBehaviour
     private Transform spawnpoint;
     [SerializeField] string spawnpointName;
 
-    
+    int tryAFew = 50;
 
     void Start()
     {
         spawnpoint = GameObject.Find(spawnpointName).transform;
         this.gameObject.transform.position = spawnpoint.position;
+    }
+
+    void Update()
+    {
+        if(tryAFew != 0)
+        {
+            tryAFew--;
+            spawnpoint = GameObject.Find(spawnpointName).transform;
+            this.gameObject.transform.position = spawnpoint.position;
+            
+        }
     }
 }
